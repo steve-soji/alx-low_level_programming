e9d3d1dc570e877e0f299f56db15d58cbded8be7
steve-soji <sojipris@yahoo.fr>
2023-02-23 04:22:29 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,18 +10,18 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = '0' ; i <= '9' ; ++i)
+	for (i = 0 ; i <= 9 ; ++i)
 	{
-		for (j = '1' ; j <= '9' ; ++j)
+		for (j = 1 ; j <= 9 ; ++j)
 		{
 			if (i != j && i < j)
 			{
-				putchar(i);
-				putchar(j);
+				putchar(i + '0');
+				putchar(j + '0');
 					if (i + j != 17)
 					{
-					putchar(',');
-					putchar(' ');
+						putchar(',');
+						putchar(' ');
 					}
 			}
 		}
