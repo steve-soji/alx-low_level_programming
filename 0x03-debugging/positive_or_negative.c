3884279f2596b363699e74c7b0ef028be7576b68
steve-soji <sojipris@yahoo.fr>
2023-02-26 19:30:20 -0800

    0-main.c, main.h

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - checks sign of a number
+ *
+ * @i: the number to be checked
+ *
+ * Return: Always 0
+ */
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
+}
